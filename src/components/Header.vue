841ee05b04af9b856c9d5a5fa79c76747174b751
baptistemrt <contact@baptistemrt.fr>
2020-12-12 00:49:07 +0100

    Add css header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,14 @@
 <template>
-  <h1>Todo. Build with <img src="../assets/logo.png" style="width: 50px;" /></h1>
+  <h1 class="title">Todos. Build with <img src="../assets/logo.png" style="width: 50px;" /></h1>
 </template>
 
 <style lang="scss">
-  
+  .title {
+    background-color: #eee;
+    display: flex;
+    align-items: center;
+    font-size: 2rem;
+    padding: 15px;
+    border-radius: 3px;
+  }
 </style>
