bfce71cada394a9e94832a537e49ae6daa0177fe
baptistemrt <contact@baptistemrt.fr>
2020-12-12 00:48:42 +0100

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <Header />
-    <Todo />
+    <section class="container">
+      <Header />
+      <Todo />
+    </section>
+    <Footer />
   </div>
 </template>
 
@@ -9,23 +12,34 @@
 
 import Header from './components/Header';
 import Todo from './components/Todo';
+import Footer from './components/Footer';
 
 export default {
   name: 'App',
   components: {
     Header,
-    Todo
+    Todo,
+    Footer
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  html, body, #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    background: linear-gradient(to left, #ED4C67, #B53471, #833471, #6F1E51);
+  }
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    margin-top: 75px;
+  }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="footer">
+    <p>Made with ❤️ by <a href="https://baptistemrt.fr" target="_blank">baptistemrt</a></p>
+  </section>
+</template>
+
+<style lang="scss">
+  .footer {
+    position: fixed;
+    width: 100%;
+    bottom: 0;
+    p {
+      font-weight: bold;
+      text-align: center;
+      font-size: .8rem;
+      color: #FFF;
+      a {
+        color: #FFF;
+      }
+    }
+  }
+</style>
