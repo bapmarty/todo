2f19ec4afb276e077dc8985b9a6b7a4f79d9d9e5
baptistemrt <contact@baptistemrt.fr>
2020-12-11 16:08:59 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header />
+    <Todo />
+  </div>
+</template>
+
+<script>
+
+import Header from './components/Header';
+import Todo from './components/Todo';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Todo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Todo. Build with <img src="../assets/logo.png" style="width: 50px;" /></h1>
+</template>
+
+<style scoped>
+
+</style>
